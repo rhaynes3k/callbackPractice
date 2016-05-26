e4852c7f6bf1dd2d664df450fc252ad410f6dea0
rhaynes3k <rhaynes3k@gmail.com>
2016-05-26 18:08:03 -0500

    2ndcommit

--- callbackPractice.js
+++ callbackPractice.js
@@ -1,16 +1,16 @@
 /* In this repo your job is to write functions to make each function call work properly.
-Below is a sample problem 
+Below is a sample problem
 
   //code here for sayHi
 
    sayHi('Hi Katie', function(thingToSay){
       alert(thingToSay);
    });
-   
 
-and what you should write is the sayHi function that makes the code above work, 
-    
-    
+
+and what you should write is the sayHi function that makes the code above work,
+
+
    var sayHi = function(str, cb){
     cb(str);
    }
@@ -18,14 +18,16 @@ and what you should write is the sayHi function that makes the code above work,
    sayHi('Hi Katie', function(thingToSay){
       alert(thingToSay); //should alert ('Hi Katie')'
    });
-    
-    
+
+
 */
 
 
 
-  //Code Here for first
-  
+var first = function(arr, cb) {
+  cb(arr[0]);
+};//Code Here for first
+
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 first(names, function(firstName){
@@ -39,7 +41,9 @@ first(names, function(firstName){
 
 
 
-  //Code Here for last
+var last = function(arr, cb) {
+  cb(arr[arr.length - 1]);
+};//Code Here for last
 
 last(names, function(lastName){
   console.log('The last name in names is ' + lastName);
@@ -56,7 +60,9 @@ last(names, function(lastName){
 
 
 
-  //Code Here for multiply
+  var multiply = function(num1, num2, cb) {
+    cb(num1 * num2);
+  };//Code Here for multiply
 
 multiply(4, 3, function(answer){
   console.log('The answer is ' + answer); //should console.log 12
@@ -72,7 +78,15 @@ multiply(4, 3, function(answer){
 
 
 
-  //Code Here for contains
+var names = ["John", "Colt"]
+var contains = function(nms, nm, cb) {
+    for(var i = 0; i < nms.length; i++){
+      if (nm === nms[i]) {
+        return true;
+      }
+     }
+    return false;
+  }//Code Here for contains
 
 contains(names, 'Colt', function(result){
   if(result === true){
@@ -81,7 +95,6 @@ contains(names, 'Colt', function(result){
     console.log('Colt is not in the array');
   }
 });
-
 
 
 
@@ -146,5 +159,5 @@ var users = [
 ];
 
 getUserById(users, '16t', function(user){
-  console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address); 
+  console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address);
 });
